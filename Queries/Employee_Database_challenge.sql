f2c220a19eaa99f85271ceb89cfdfb34d1a5df3f
tanzimamin2 <93144225+tanzimamin2@users.noreply.github.com>
2021-12-16 15:49:16 -0500

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,106 @@
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+CREATE TABLE employees (
+     emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, from_date)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Find the number of retiring employees by title (Steps 1 to 7)
+SELECT em.emp_no, 
+       em.first_name, 
+       em.last_name,
+	   ti.title, 
+       ti.from_date, 
+       ti.to_date
+	INTO retirement_titles
+	FROM employees AS em
+	LEFT JOIN titles as ti
+	ON (em.emp_no = ti.emp_no)
+	WHERE (em.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	ORDER BY em.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows (Steps 8 to 15)
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Retrieve the number of employees by their most recent job title who are about to retire (Steps 16 to 22)
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.title) DESC;
+
+-- Deliverable 2: Employees Eligible for the Mentorship Program (Steps 1 to 11)
+-- Employees born 1965-01-01 to 1965-12-31
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, ti.to_date DESC;
+
+SELECT * FROM retirement_titles;
+
